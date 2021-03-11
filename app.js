48e29f7d153649f4cfcd038e9b806bf982469aca
rodawg <r.dadds94@gmail.com>
2021-03-11 08:58:26 +0000

    Added packages nodemon (to improve development cycle) and morgan (for logging).=

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const morgan =  require('morgan');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 const app = express();
+
+app.use(morgan('dev'))
 
 // Set up a Middleware
 app.use('/products', productRoutes);
