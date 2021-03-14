20026cbc2325bf17f5ad083b0c9598d235d67005
rodawg <r.dadds94@gmail.com>
2021-03-14 21:44:46 +0000

    Added and implemented body parser.

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -11,8 +11,13 @@ router.get('/',
 
 router.post('/',
     (req, res, next) => {
+        const order = {
+            productId: req.body.productId,
+            quantity: req.body.quantity
+        }
         res.status(201).json({
-            message: "Your order was created"
+            message: "Your order was created",
+            order: order
         })
     })
 
--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const morgan =  require('morgan');
+const bodyParser = require('body-parser');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 const app = express();
 
 app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false})); // false = only support simple bodies for url encoded data
+app.use(bodyParser.json());
 
 // Set up a Middleware
 app.use('/products', productRoutes);
