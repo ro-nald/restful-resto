f4c4cefbcaa45de84b48ea4e45d1fd6f20d568be
rodawg <r.dadds94@gmail.com>
2021-03-14 22:22:20 +0000

    Handle CORS (Cross-Origin Resource Sharing) headers.

--- app.js
+++ app.js
@@ -10,6 +10,22 @@ app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false})); // false = only support simple bodies for url encoded data
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // * gives access to any origin - for APIs, want to do this.
+    // Allow anything (*) or 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') { // Browser will always send am 'OPTIONS' request prior to a POST or PUT request.
+        res.header('Access-Control-Allow-Methods',
+            'PUT, POST, PATCH, DELETE, GET'
+        ); // Add additional header, about permitted methods
+        return res.status(200).json({}); // Return empty JSON response, as the headers contain the information.
+    }
+    next(); // Call next so that other routes can take over
+})
+
 // Set up a Middleware
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
