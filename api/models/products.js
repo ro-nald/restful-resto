db3e93a9bc09a748156ac2a4fc07cb0c14164695
rodawg <r.dadds94@gmail.com>
2021-03-24 21:31:22 +0000

    Require 'name' and 'price' for the Product schema.

--- api/models/products.js
+++ api/models/products.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 // Define Schema (layout of data object)
 const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: String,
-    price: Number
+    name: { type: String, required: true },
+    price: { type: Number, required: true }
 });
 
 // Define model - provides methods to construct the object
