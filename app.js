a7c61e36896b4393a406554a2ffee65281fc3b60
rodawg <r.dadds94@gmail.com>
2021-04-02 17:04:00 +0100

    Added user sign up functionality

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/users');
 
 const app = express();
 
@@ -45,6 +46,7 @@ app.use((req, res, next) => {
 // Set up a Middleware
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/users', userRoutes)
 
 app.use((req, res, next) => {
     const error = new Error('Not Found');
